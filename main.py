1b4785ab8aeee40ddacb41a03342cb900cdf8be2
Norbert Lewicz <lewy054@gmail.com>
2021-04-21 15:05:26 +0200

    Github api integration

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 from flask import Flask
+import requests
+import json
+
 app= Flask(__name__)
+
+def call_github(username):
+    data_all = {}
+    github_api = 'https://api.github.com/users'
+    response = requests.get(f'{github_api}/{username}/repos').json()
+    for r in response:
+        data = {'watchers': r['watchers_count']}
+        data_all[r['name']] = data
+    return json.dumps(data_all)
 
 @app.route('/')
 def index():
-    return 'index'
+    return call_github('allegro')
 
 if __name__ == "__main__":
     app.run(debug=True)
