684fb12012f8c4379712a5055f5305d63dd764bf
Norbert Lewicz <lewy054@gmail.com>
2021-04-23 04:27:19 +0200

    Add validation

--- main.py
+++ main.py
@@ -6,41 +6,43 @@ app = Flask(__name__)
 
 
 def call_github(username):
-    github_api = 'https://api.github.com/users'
-    response = requests.get(f'{github_api}/{username}/repos').json()
-    return response
+    try:
+        github_api = 'https://api.github.com/users'
+        response = requests.get(f'{github_api}/{username}/repos')
+    except:
+        return Response(status=404)
+    else:
+        return response
 
 
-def get_repos_info(username):
-    data_all = {}
-    response = call_github(username)
-    for r in response:
-        data = {'watchers': r['watchers_count']}
-        data_all[r['name']] = data
-    return json.dumps(data_all)
-
-
-def count_user_stars(username):
-    stars = 0
-    response = call_github(username)
-    for r in response:
-        stars += r['watchers_count']
-    return json.dumps(stars)
-
-
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
     return 'test'
 
 
-@app.route('/repos/<user>')
-def repos(user):
-    return Response(get_repos_info(user), status=200, mimetype='application/json')
+@app.route('/repos/<username>')
+def repos(username):
+    data_all = {}
+    response = call_github(username)
+    if(response.status_code == 200):
+        for r in response.json():
+            data = {'watchers': r['watchers_count']}
+            data_all[r['name']] = data
+        return Response(json.dumps(data_all), status=200, mimetype='application/json')
+    else:
+        return Response(status=400)
 
 
-@app.route('/stars/<user>')
-def stars(user):
-    return Response(count_user_stars(user), status=200, mimetype='application/json')
+@app.route('/stars/<username>')
+def stars(username):
+    stars = 0
+    response = call_github(username)
+    if(response.status_code == 200):
+        for r in response.json():
+            stars += r['watchers_count']
+        return Response(json.dumps(stars), status=200, mimetype='application/json')
+    else:
+        return Response(status=400)
 
 
 if __name__ == "__main__":
